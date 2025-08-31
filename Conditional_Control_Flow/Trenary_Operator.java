7efa20680f4d2e76652767f8ad711f7de6afb930
Nancy Singh <097nancysingh@gmail.com>
2025-08-31 12:03:35 +0530

    Add files via upload

---
+++ Conditional_Control_Flow/Trenary_Operator.java
@@ -0,0 +1,14 @@
+package Conditional_Control_Flow;
+
+public class Trenary_Operator {
+
+	public static void main(String[] args) {
+		// using the ternary operator -> this takes the condition and based on the condition executes the value
+		
+		int a= 10;
+		int b = 20;
+		System.out.println((a>b ? a+" is greater value than "+b:b +" is the greater value than "+a));
+
+	}
+
+}
