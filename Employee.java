fa0e42fe944f7b6d572762a0aa027a60f957f6aa
Nancy Singh <097nancysingh@gmail.com>
2025-09-01 09:42:08 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package constructor;
+
+public class Employee {
+	
+	int id;
+	double esalary;
+	
+	Employee(int id,double salary)
+	eid=id;
+	esalary=salary;
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		e1.Employee=new Employee(2345,100000);
+		System.out.println("eid :"+e1.id +", easalry : "+e1.salary);
+		
+
+	}
+
+}
